35ec74c333cae62443db0118e4cf709ecfad25de
mfallon <mtfallon91@gmail.com>
2024-01-27 10:07:28 -0600

    add double dabble and improve display

---
+++ seven_segment/hdl/double_dabble.sv
@@ -0,0 +1,82 @@
+`timescale 1ns / 1ps
+
+module double_dabble #(
+    parameter NUM_BITS = 14
+) (
+    input  wire                 clk,
+    input  wire                 rst,
+    input  wire  [NUM_BITS-1:0] binary_in,
+    input  wire                 binary_in_valid,
+    output logic [15:0]         packed_bcd_out,
+    output logic                packed_bcd_out_valid
+);
+
+    typedef enum {
+        IDLE,
+        SHIFT,
+        CHECK_LP,
+        ADD,
+        DONE
+    } state_t;
+
+    state_t                          state;
+    logic     [NUM_BITS-1:0]         bin;
+    logic     [$clog2(NUM_BITS)-1:0] count;
+    logic     [15:0]                 pckd_bcd;
+
+    assign packed_bcd_out = pckd_bcd;
+    
+    always_ff @(posedge clk) begin
+        if(rst == 1) begin
+            state                <= IDLE;
+            packed_bcd_out_valid <= '0;
+            bin                  <= 'x;
+            count                <= 'x;
+            pckd_bcd             <= 'x;
+        end else begin
+            packed_bcd_out_valid <= '0;
+            case(state)
+                IDLE: begin
+                    state    <= IDLE;
+                    pckd_bcd <= 0;
+                    count    <= 0;
+                    bin      <= binary_in;
+
+                    if(binary_in_valid == 1) begin
+                        state <= SHIFT;
+                    end
+                end
+
+                SHIFT: begin
+                    state       <=  CHECK_LP;
+                    pckd_bcd    <=  pckd_bcd << 1;
+                    bin         <=  bin << 1;
+                    count       <=  count + 1;
+                end
+
+                CHECK_LP: begin
+                    state <= ADD;
+
+                    if(count >= NUM_BITS) begin
+                        state <= DONE;
+                    end
+                end
+
+                ADD: begin
+                    state <= SHIFT;
+                    if(pckd_bcd[15:12]>4) begin pckd_bcd[15:12] <= pckd_bcd[15:12]+3 end;
+                    if(pckd_bcd[11:8]>4)  begin pckd_bcd[11:8]  <= pckd_bcd[11:8]+3  end;
+                    if(pckd_bcd[7:4]>4)   begin pckd_bcd[7:4]   <= pckd_bcd[7:4]+3   end;
+                    if(pckd_bcd[3:0]>4)   begin pckd_bcd[3:0]   <= pckd_bcd[3:0]+3   end;
+                end
+
+                DONE: begin
+                    state                <= IDLE;
+                    packed_bcd_out_valid <= 1'b1;
+                end
+
+                default: state <= IDLE;
+            endcase
+        end
+    end
+endmodule
--- seven_segment/hdl/seven_segment_display.sv
+++ seven_segment/hdl/seven_segment_display.sv
@@ -1,20 +1,20 @@
 `timescale 1ns / 1ps
-`define DATA_BITS ((HEX == 1) ? 4*NUM_DIGITS : $clog2(10*NUM_DIGITS))
+`define DATA_BITS ((HEX == 1) ? 4*NUM_DIGITS : $clog2(10**NUM_DIGITS))
 
 module seven_segment_display #(
     parameter int NUM_DIGITS    = 4,
     parameter int HEX           = 0,
-    parameter int CLK_FREQ      = 125000000
+    parameter int CLK_FREQ      = 125 //MHz
     )(
     input  wire                   clk,
     input  wire                   rst,
-    input  wire  [NUM_DIGITS-1:0] enable,
     input  wire  [`DATA_BITS-1:0] data_in,
+    output logic [NUM_DIGITS-1:0] enable,
     output logic [7:0]            led_out
     );
 
-    localparam int TARGET_PERIOD = 16000000; //ns
-    localparam int CLOCK_CYCLES  = TARGET_PERIOD*CLK_FREQ;
+    localparam int TARGET_PERIOD = 16; //ms
+    localparam int CLOCK_CYCLES  = TARGET_PERIOD*CLK_FREQ*100;
 
     logic [$clog2(CLOCK_CYCLES)-1:0] refresh_counter;
     logic [$clog2(NUM_DIGITS)-1:0]   enable_select;
@@ -37,25 +37,43 @@ module seven_segment_display #(
         enable = '1;
         inc_select = (refresh_counter == '1);
         enable[enable_select] = 1'b0;
+
+        if (HEX == 0) begin
+            case (data_in)
+                0:       led_out = 8'b00000011;
+                1:       led_out = 8'b10011111;
+                2:       led_out = 8'b00100101;
+                3:       led_out = 8'b00001101;
+                4:       led_out = 8'b10011001;
+                5:       led_out = 8'b01001001;
+                6:       led_out = 8'b01000001;
+                7:       led_out = 8'b00011111;
+                8:       led_out = 8'b00000001;
+                9:       led_out = 8'b00001001;
+                default: led_out = '1;
+            endcase
+        end else begin
+            case (data_in)
+                0:       led_out = 8'b00000011;
+                1:       led_out = 8'b10011111;
+                2:       led_out = 8'b00100101;
+                3:       led_out = 8'b00001101;
+                4:       led_out = 8'b10011001;
+                5:       led_out = 8'b01001001;
+                6:       led_out = 8'b01000001;
+                7:       led_out = 8'b00011111;
+                8:       led_out = 8'b00000001;
+                9:       led_out = 8'b00001001;
+                10:      led_out = 8'b00010001;
+                11:      led_out = 8'b11000001;
+                12:      led_out = 8'b01100011;
+                13:      led_out = 8'b10000101;
+                14:      led_out = 8'b01100001;
+                15:      led_out = 8'b01110001;
+                default: led_out = '1;
+            endcase
+        end
     end
 
-    generate
-        if (HEX == 0) begin
-            always_comb begin
-                case (data_in)
-                    0:       led_out = 8'b00000011;
-                    1:       led_out = 8'b10011111;
-                    2:       led_out = 8'b00100101;
-                    3:       led_out = 8'b00001101;
-                    4:       led_out = 8'b10011001;
-                    5:       led_out = 8'b01001001;
-                    6:       led_out = 8'b01000001;
-                    7:       led_out = 8'b00011111;
-                    8:       led_out = 8'b00000001;
-                    9:       led_out = 8'b00001001;
-                    default: led_out = '1;
-                endcase
-            end
-        end
-    endgenerate
 endmodule
+
